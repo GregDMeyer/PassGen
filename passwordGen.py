612705d8328cb5fab5b9076b201e20209505498a
Greg Meyer <gregory.meyer@gmail.com>
2016-11-02 11:15:35 -0700

    Minor changes:  - change how to deal with variable length elements  - allow running as standalone

--- passwordGen.py
+++ passwordGen.py
@@ -1,5 +1,5 @@
 
-from Crypto.Random.random import choice
+from Crypto.Random.random import choice, sample
 from string import printable, ascii_lowercase, ascii_letters, digits
 from math import log, ceil, floor
 from os.path import isfile
@@ -13,7 +13,7 @@ def max_len_exception(max_length,min_length,min_entropy):
 
 def make_pass(pool='chars',
               min_entropy=20,
-              min_length=6,
+              min_length=8,
               max_length=30,
               filter_fn=None,
               mod_fn=None,
@@ -34,7 +34,7 @@ def make_pass(pool='chars',
         pool = printable.strip()
 
     elif pool == 'lowercase':
-        pool = lowercase
+        pool = ascii_lowercase
 
     elif pool == 'alphanumeric':
         pool = ascii_letters + digits
@@ -80,40 +80,48 @@ def make_pass(pool='chars',
     # only need to do this if elements are not length 1
     if any([len(x) > 1 for x in pool]):
 
-        avg_req_element_length = floor( (max_length - len(join_str)*(min_elements - 1)) / min_elements )
+        avg_req_element_length = float(max_length - len(join_str)*(min_elements - 1)) / min_elements
 
         # figure out distribution of lengths
         len_dist = [0]*(max([len(x) for x in pool]))
         for x in pool:
             len_dist[len(x)-1] += 1
 
-        # now figure out the distribution to get the right average
-        
+        while True:
+            tot_len = 0
+            n_elements = 0
+            # now figure out the distribution to get the right average
+            for n,val in enumerate(len_dist):
+                if float(tot_len+(n+1)*val)/(n_elements+val) > avg_req_element_length: # average has exceeded our max
+                    break
+                tot_len += (n+1)*val
+                n_elements += val
 
-        # remove elements of the pool to achieve correct average length (so we probably nail max_length)
-        while avg_req_element_length < float(sum([len(x) for x in pool]))/len(pool):
+            # figure out how many we need of that last category to bring the average up to what we want
+            n_last = int(((avg_req_element_length - float(tot_len/n_elements))/(n+1 - avg_req_element_length)) * n_elements)
 
-            max_element_length = max([len(x) for x in pool])
+            if n+1 == len(len_dist) and n_last > len_dist[n]: # then we are using everything. no need to trim
+                n_last = len_dist[n]
 
-            # get rid of the longest ones
-            pool = {x for x in pool if len(x) < max_element_length}
-            if len(pool) == 0:
-                raise max_len_exception(max_length, min_length, min_entropy)
+            # new pool size
+            pool_size = n_elements + n_last
 
-            ind_entropy = log(len(pool), 2) # this has changed!
+            ind_entropy = log(pool_size,2)
+            if min_elements == int(ceil(min_entropy/ind_entropy)): # don't need to recalculate
+                break
             min_elements = int(ceil(min_entropy/ind_entropy))
-            avg_req_element_length = floor( (max_length - len(join_str)*(min_elements - 1)) / min_elements )
 
-        if verbose:
-            print('Average pool element length:', float(sum([len(x) for x in pool]))/len(pool))
+        pool = {x for x in pool if len(x) < n+1} | set( sample([x for x in pool if len(x) == n+1], n_last))
 
     # need to switch back to a list for the random module
     pool = list(pool)
 
+    # no reason to limit entropy by not allowing repeats
     element_lst = [choice(pool) for i in range(min_elements)]
     # we might get unlucky and have it too long. keep trying until we don't
+    # this reduces the entropy a bit. But I think it's not too bad
     while len(join_str.join(element_lst)) > max_length:
-        # no reason to limit entropy by not allowing repeats
+        print('Redoing... too long')
         element_lst = [choice(pool) for i in range(min_elements)]
 
     while len(join_str.join(element_lst)) < min_length:
@@ -123,9 +131,35 @@ def make_pass(pool='chars',
         raise max_len_exception(max_length, min_length, min_entropy)
 
     if verbose:
-        print('Password: ', join_str.join(element_lst))
+        print('Password: ', join_str.join(element_lst),'\n')
+        if any([len(x) > 1 for x in pool]):
+            print('Average pool element length: ', float(sum([len(x) for x in pool]))/len(pool))
         print('Bits entropy: ', ind_entropy*len(element_lst))
         print('Length: ', len(join_str.join(element_lst)))
         print('N. elements: ', len(element_lst))
 
     return join_str.join(element_lst)
+
+
+if __name__ == "__main__":
+
+    from sys import argv
+
+    kwargs = {'verbose':True}
+    
+    for n,arg in enumerate(argv):
+        if arg[0] == '-':
+            try:
+                kwargs[arg.lstrip('-')] = int(argv[n+1])
+            except ValueError:
+                try:
+                    kwargs[arg.lstrip('-')] = float(argv[n+1])
+                except ValueError:
+                    pass
+
+            if arg.lstrip('-') not in kwargs:
+                kwargs[arg.lstrip('-')] = argv[n+1]
+            pass
+        pass
+
+    make_pass(**kwargs)
